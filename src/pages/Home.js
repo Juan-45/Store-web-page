fefd131a8771d5fe7475aa64f5bd2a50660d9177
Juan-45 <quijuan12@gmail.com>
2022-01-01 15:47:32 -0300

    Use Typography

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import nuts from "images/nuts.jpg";
 import driedFruits from "images/driedFruits.jpg";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   const ParallaxContainer = styled("div")({
@@ -24,6 +25,7 @@ const Home = () => {
     //component-style
     backgroundImage: `url(${wood})`,
     backgroundSize: "cover",
+    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     filter: "brightness(50%)",
     //object
@@ -44,6 +46,7 @@ const Home = () => {
     //component-style
     backgroundImage: `url(${img})`,
     backgroundSize: "cover",
+    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     filter: "sepia(30%)",
     //object
@@ -79,7 +82,6 @@ const Home = () => {
     color: "white",
     opacity: "80%",
     width: "70%",
-    height: "80%",
   }));
 
   return (
@@ -92,7 +94,7 @@ const Home = () => {
         img={nuts}
         spacing={0}
       >
-        <PresentationContainer container item xs={12} sm={6}>
+        <PresentationContainer container item sm={12} md={6}>
           <PaperContainer elevation={16} variant="darkShadow">
             <Grid
               container
@@ -101,28 +103,19 @@ const Home = () => {
               alignItems="center"
               sx={{
                 p: 5,
-                width: "100%",
-                height: "100%",
+                //  width: "100%",
+                // height: "100%",
               }}
             >
-              <h1
-                style={{
-                  fontSize: "36px",
-                }}
-              >
-                Hello world
-              </h1>
-              <p
-                style={{
-                  fontSize: "26px",
-                }}
-              >
+              <Typography variant="h3" gutterBottom={true}>
+                Lorem ipsum
+              </Typography>
+              <Typography paragraph={true} align="justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                 interdum dapibus mauris eget maximus. Suspendisse potenti. Cras
                 tristique, erat nec pharetra lobortis, erat urna bibendum erat,
-                ac feugiat dolor tortor ac felis. Donec a gravida massa. Nunc
-                hendrerit tellus vel tellus convallis, et auctor orci imperdiet.
-              </p>
+                ac feugiat dolor tortor ac felis. Donec a gravida massa.
+              </Typography>
             </Grid>
           </PaperContainer>
         </PresentationContainer>
@@ -133,7 +126,7 @@ const Home = () => {
         alignItems="flex-start"
         img={driedFruits}
       >
-        <PresentationContainer container item xs={12} sm={6}>
+        <PresentationContainer container item sm={12} md={6}>
           <PaperContainer elevation={16} variant="darkShadow">
             <Grid
               container
@@ -146,24 +139,15 @@ const Home = () => {
                 height: "100%",
               }}
             >
-              <h1
-                style={{
-                  fontSize: "36px",
-                }}
-              >
-                Hello world
-              </h1>
-              <p
-                style={{
-                  fontSize: "26px",
-                }}
-              >
+              <Typography variant="h3" gutterBottom={true}>
+                Lorem ipsum
+              </Typography>
+              <Typography paragraph={true} align="justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
                 interdum dapibus mauris eget maximus. Suspendisse potenti. Cras
                 tristique, erat nec pharetra lobortis, erat urna bibendum erat,
-                ac feugiat dolor tortor ac felis. Donec a gravida massa. Nunc
-                hendrerit tellus vel tellus convallis, et auctor orci imperdiet.
-              </p>
+                ac feugiat dolor tortor ac felis. Donec a gravida massa.
+              </Typography>
             </Grid>
           </PaperContainer>
         </PresentationContainer>
