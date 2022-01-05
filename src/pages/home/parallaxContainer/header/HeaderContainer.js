736fbe4839a2130084d7dedc0b852b23a9f4f0d8
Juan-45 <quijuan12@gmail.com>
2022-01-04 23:52:42 -0300

    Move as a separated file

---
+++ src/pages/home/parallaxContainer/header/HeaderContainer.js
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+
+const HeaderContainer = styled(Grid)({
+  height: "100vh",
+  paddingTop: "20vh",
+  zIndex: "1",
+  position: "relative",
+});
+
+export default HeaderContainer;
