6fcfcf5c201cecaa11e439ee1a311251c2ee1faf
Juan-45 <quijuan12@gmail.com>
2022-01-04 23:51:49 -0300

    Move inside Header

---
+++ src/pages/home/parallaxContainer/header/HeaderBackground.js
@@ -0,0 +1,26 @@
+import { styled } from "@mui/material/styles";
+import wood from "images/wood.jpg";
+import Grid from "@mui/material/Grid";
+
+const HeaderBackground = styled(Grid)({
+  //component-style
+  backgroundImage: `url(${wood})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  filter: "brightness(50%)",
+  //object
+  width: "100vw",
+  height: "100vh",
+  zIndex: "-1",
+  //object-wrapper
+  position: "absolute",
+  top: "0",
+  right: "0",
+  bottom: "0",
+  left: "0",
+  //speed
+  transform: "translateZ(-13px) scale(14.2)",
+});
+
+export default HeaderBackground;
