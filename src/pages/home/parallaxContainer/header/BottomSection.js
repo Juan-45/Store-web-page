cba3726685302c1d9a4cdd661bb6c92e9ce71566
Juan-45 <quijuan12@gmail.com>
2022-01-04 23:52:22 -0300

    Move as a separated file

---
+++ src/pages/home/parallaxContainer/header/BottomSection.js
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+
+const BottomSection = styled(Grid)({
+  height: "20vh",
+  width: "80vw",
+});
+
+export default BottomSection;
---
+++ src/pages/home/parallaxContainer/header/TopSection.js
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+
+const TopSection = styled(Grid)({
+  height: "60vh",
+  width: "80vw",
+});
+
+export default TopSection;
