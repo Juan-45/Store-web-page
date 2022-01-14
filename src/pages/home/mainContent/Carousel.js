74be06721b31210868ac7375b4fd0ad80276ca9b
Juan-45 <quijuan12@gmail.com>
2022-01-14 14:33:26 -0300

    Update

--- src/pages/home/mainContent/Carousel.js
+++ src/pages/home/mainContent/Carousel.js
@@ -5,8 +5,6 @@ import BackButton from "./carousel/BackButton";
 import ForwardButton from "./carousel/ForwardButton";
 import CarouselContent from "./carousel/CarouselContent";
 import useCarousel from "./carousel/useCarousel";
-import useRefCallback from "hooks/useRefCallback";
-import { useCallback } from "react";
 
 const Carousel = ({ children }) => {
   const {
@@ -20,20 +18,8 @@ const Carousel = ({ children }) => {
     handleTouchMove,
     goForwardHandler,
     goBackHandler,
+    ref,
   } = useCarousel(children.length);
-
-  const add = useCallback((node) => {
-    const nodeList = node.firstChild.childNodes;
-    nodeList.forEach((element) => {
-      element.addEventListener("click", () => console.log("dot clicked"));
-    });
-  }, []);
-
-  const cleanUp = useCallback((currentRef) => {
-    currentRef = null;
-  }, []);
-
-  const [ref] = useRefCallback(add, cleanUp);
 
   return (
     <Container>
