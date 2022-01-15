b4d2755f317b5377fcc154e7e13c997bad89beaf
Juan-45 <quijuan12@gmail.com>
2022-01-15 09:37:47 -0300

    Update

--- src/hooks/useCheckLandScape.js
+++ src/hooks/useCheckLandScape.js
@@ -1,13 +1,26 @@
-import { /*useState, */ useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useCheckLandScape = () => {
+  const [isOnLandScape, setIsOnLandScape] = useState(false);
+
   useEffect(() => {
-    window.document.addEventListener(
-      "orientationchange",
-      () => alert(window.orientation),
+    const handleScreenRotation = (event) => {
+      const currentRotationAngle = event.currentTarget.angle;
+      if (currentRotationAngle === 0) {
+        setIsOnLandScape(false);
+      } else if (currentRotationAngle === 90 || currentRotationAngle === -90) {
+        setIsOnLandScape(true);
+      }
+    };
+
+    window.screen.orientation.addEventListener(
+      "change",
+      handleScreenRotation,
       false
     );
   }, []);
+
+  return { isOnLandScape };
 };
 
 export default useCheckLandScape;
