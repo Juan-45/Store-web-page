c51f98271dc859ca3cb7eab89714c75693effd28
Juan-45 <quijuan12@gmail.com>
2022-01-28 14:16:43 -0300

    Set Routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "theme";
 import Home from "pages/Home";
+import MainContent from "pages/home/MainContent";
+import Contact from "pages/Contact";
+import About from "pages/About";
+import SignUp from "pages/SignUp";
+import StorePickUp from "pages/StorePickUp";
+import Products from "pages/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-//TODO set min width for large screen and no-effects for smartphone screens
 //TODO comprobar layout en laptop
 //TODO set all imports from MUI to {}
+//TODO Store-web-page should be /home
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/Store-web-page" element={<Home />}>
+              <Route path="" element={<MainContent />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="about" element={<About />} />
+              <Route path="storepickup" element={<StorePickUp />} />
+              <Route path="products" element={<Products />} />
+              <Route
+                path="*"
+                element={
+                  <div
+                    style={{
+                      paddingTop: "300px",
+                      height: "100vh",
+                      background: "gray",
+                    }}
+                  >
+                    <h1>PAGE NOT FOUND</h1>
+                  </div>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </ThemeProvider>
   );
