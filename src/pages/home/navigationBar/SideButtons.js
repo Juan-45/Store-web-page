7efc6b27725b816a2cdc8510c69c576722eeed4e
Juan-45 <quijuan12@gmail.com>
2022-01-28 17:33:02 -0300

    Add navigation handler

--- src/pages/home/navigationBar/SideButtons.js
+++ src/pages/home/navigationBar/SideButtons.js
@@ -2,8 +2,12 @@ import HomeIcon from "@mui/icons-material/Home";
 import MobileButton from "../MobileButton";
 import PersonIcon from "@mui/icons-material/Person";
 import { Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const SideButtons = ({ sx }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate("/Store-web-page", { replace: true });
   return (
     <Grid
       container
@@ -14,7 +18,7 @@ const SideButtons = ({ sx }) => {
         ...sx,
       }}
     >
-      <MobileButton sx={{ marginRight: "15px" }}>
+      <MobileButton sx={{ marginRight: "15px" }} onClick={handleClick}>
         <HomeIcon fontSize="medium" />
       </MobileButton>
       <MobileButton sx={{ marginRight: "30px" }}>
