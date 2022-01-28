867521e76db371774c06b6d7c427dfcbb88b2e32
Juan-45 <quijuan12@gmail.com>
2022-01-28 14:14:13 -0300

    Create new pages

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+
+const Products = () => {
+  return (
+    <Box sx={{ height: "100vh", background: "gray", paddingTop: "50vh" }}>
+      <Typography variant="h1">Productos</Typography>
+    </Box>
+  );
+};
+
+export default Products;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+
+const SignUp = () => {
+  return (
+    <Box sx={{ height: "100vh", background: "gray", paddingTop: "50vh" }}>
+      <Typography variant="h1">Registrarse</Typography>
+    </Box>
+  );
+};
+
+export default SignUp;
