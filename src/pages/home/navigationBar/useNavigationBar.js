478f328324897602d9637c690962c3c93f33e5a7
Juan-45 <quijuan12@gmail.com>
2022-01-28 14:13:49 -0300

    Fix bad re-render on quick scrolling

--- src/pages/home/navigationBar/useNavigationBar.js
+++ src/pages/home/navigationBar/useNavigationBar.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const useNavigationBar = (shouldDisplay) => {
   const [shouldRemoveComponent, setShouldRemoveComponent] = useState(false);
+  const timerID = useRef();
 
   useEffect(() => {
-    const timer = () => {
-      if (!shouldDisplay) {
-        setTimeout(() => setShouldRemoveComponent(true), 420);
-      } else setShouldRemoveComponent(false);
-    };
-    timer();
-    clearTimeout(timer);
+    const timer = () =>
+      window.setTimeout(() => setShouldRemoveComponent(true), 1000);
+    if (!shouldDisplay) {
+      timerID.current = timer();
+    } else {
+      window.clearTimeout(timerID.current);
+      setShouldRemoveComponent(false);
+    }
   }, [shouldDisplay]);
 
   return {
