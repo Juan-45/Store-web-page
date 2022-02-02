d8088c6286d5417ee4fca7718931ef470414e6d7
Juan-45 <quijuan12@gmail.com>
2022-02-02 00:52:55 -0300

    Fix layout

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -8,16 +8,15 @@ const PageContainer = ({ children }) => {
       alignItems="flex-start"
       justifyContent="center"
       sx={{
-        height: "inherit",
         background: theme.palette.background.light,
         color: theme.palette.text.secondary,
       }}
     >
-      <Grid item xs={10} md={9} lg={6} xl={5} sx={{ height: "inherit" }}>
+      <Grid item xs={10} md={9} lg={6} xl={5}>
         <Box
           sx={{
-            height: "inherit",
             paddingTop: { xs: "156px", md: "158.5px" },
+            paddingBottom: "100px",
           }}
         >
           {children}
