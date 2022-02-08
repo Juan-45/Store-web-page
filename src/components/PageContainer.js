ce8d48daa1fbbf10925bcc266e243ec95d60c8fc
Juan-45 <quijuan12@gmail.com>
2022-02-08 11:18:02 -0300

    Fix layout
    No matter the content, the height always is at least 100vh

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -10,13 +10,14 @@ const PageContainer = ({ children }) => {
       sx={{
         background: theme.palette.background.light,
         color: theme.palette.text.secondary,
+        minHeight: "100vh",
       }}
     >
       <Grid item xs={10} md={9} lg={6} xl={5}>
         <Box
           sx={{
             paddingTop: { xs: "156px", md: "158.5px" },
-            paddingBottom: "100px",
+            paddingBottom: "200px",
           }}
         >
           {children}
