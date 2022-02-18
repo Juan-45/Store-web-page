f38b42602da728c1ee07c4fedf1528a7db598103
Juan-45 <quijuan12@gmail.com>
2022-02-18 16:34:44 -0300

    Add simple map instance

--- src/pages/StorePickUp.js
+++ src/pages/StorePickUp.js
@@ -1,11 +1,24 @@
-import { Box } from "@mui/material";
-import { Typography } from "@mui/material";
+import PageContainer from "components/PageContainer";
+import PageTitle from "components/PageTitle";
+import MapContainer from "./storePickUp/MapContainer";
+import { useEffect, useRef } from "react";
 
-const StorePickUp = () => {
+const StorePickUp = ({ isUserLogged, handler }) => {
+  const timerID = useRef();
+
+  useEffect(() => {
+    const timer = () => window.setTimeout(() => handler(), 1000);
+    if (!isUserLogged) {
+      timerID.current = timer();
+    }
+    return () => window.clearTimeout(timerID.current);
+  }, [handler, isUserLogged]);
+
   return (
-    <Box sx={{ height: "100vh", background: "gray", paddingTop: "50vh" }}>
-      <Typography variant="h1">Buscar Sucursal</Typography>
-    </Box>
+    <PageContainer>
+      <PageTitle>Encuentra nuestras sucursales</PageTitle>
+      {isUserLogged ? <MapContainer /> : null}
+    </PageContainer>
   );
 };
 
