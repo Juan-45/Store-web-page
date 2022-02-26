d749ccfb0eb11e2b90de2d956af0ac4b0f121ecf
Juan-45 <quijuan12@gmail.com>
2022-02-26 18:56:01 -0300

    Create StorePickUp page

--- src/pages/StorePickUp.js
+++ src/pages/StorePickUp.js
@@ -1,10 +1,81 @@
 import PageContainer from "components/PageContainer";
 import PageTitle from "components/PageTitle";
+import { Grid } from "@mui/material";
+import CityMenu from "./storePickUp/CityMenu";
+import GoogleMapsContainer from "./storePickUp/GoogleMapsContainer";
+import LocationsList from "./storePickUp/LocationsList";
+import { useState } from "react";
 
 const StorePickUp = () => {
+  const [currentCity, setCurrentCity] = useState("pergamino");
+
+  const locationSettings = {
+    pergamino: [
+      { zone: "Centro", address: "Pueyrredon 1154", phone: "02477-458795" },
+      {
+        zone: "Centenario",
+        address: "Juan B. Justo 1752",
+        phone: "02477-458787",
+      },
+    ],
+    rosario: [
+      { zone: "Centro", address: "Pellegrini 3200", phone: "0341-4587955" },
+      { zone: "Centro", address: "Mendoza 204", phone: "0341-4587877" },
+      { zone: "Zona Sur", address: "27 de Febrero 400", phone: "0341-4587877" },
+      { zone: "Centro", address: "Cordoba 1254", phone: "0341-4547877" },
+      { zone: "Centro", address: "Entre Rios 852", phone: "0341-4227877" },
+    ],
+    junin: [
+      { zone: "Centro", address: "Francia 1457", phone: "0236-4581155" },
+      { zone: "Centro", address: "San Juan 120", phone: "0236-4587937" },
+    ],
+    buenosAires: [
+      { zone: "Capital", address: "Av. de Mayo 4500", phone: "011-42587955" },
+      { zone: "Capital", address: "Av. Cabildo 2004", phone: "011-45487877" },
+      { zone: "Capital ", address: "27 de Febrero 400", phone: "011-45387877" },
+      { zone: "Capital", address: "Cordoba 1254", phone: "011-45478772" },
+      { zone: "Capital", address: "Av. Cabildo 852", phone: "011-42278277" },
+      { zone: "Capital", address: "Av. Cabildo 1540", phone: "011-42274877" },
+      { zone: "Capital", address: "Entre Rios 852", phone: "011-42278877" },
+      { zone: "Capital", address: "Entre Rios 254", phone: "011-42278797" },
+    ],
+  };
+
+  const handleClick = (event) => setCurrentCity(event.target.name);
+
   return (
     <PageContainer>
       <PageTitle>Encuentra nuestras sucursales</PageTitle>
+      <Grid container direction="column" rowGap="20px">
+        <CityMenu
+          handleClick={handleClick}
+          cities={[
+            { label: "Pergamino", name: "pergamino" },
+            { label: "Rosario", name: "rosario" },
+            { label: "Junín", name: "junin" },
+            { label: "Buenos Aires", name: "buenosAires" },
+          ]}
+        />
+        <Grid
+          container
+          columns="16"
+          direction="row-reverse"
+          alignItems="flex-start"
+          columnGap="20px"
+          rowGap="20px"
+          sx={{
+            flexWrap: { xs: "wrap", sm: "nowrap" },
+          }}
+        >
+          <GoogleMapsContainer item xs={16} sm={11} />
+          <LocationsList
+            item
+            xs={16}
+            sm={5}
+            locationSettings={locationSettings[currentCity]}
+          />
+        </Grid>
+      </Grid>
     </PageContainer>
   );
 };
