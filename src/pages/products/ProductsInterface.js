0fc2176bd2c0947c885c28f65af5495b280fc94c
Juan-45 <quijuan12@gmail.com>
2022-03-12 20:33:53 -0300

    Add mockedup interface

--- src/pages/products/ProductsInterface.js
+++ src/pages/products/ProductsInterface.js
@@ -1,5 +1,7 @@
 import { useParams, useSearchParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
+import GenericContainer from "components/GenericContainer";
+import ProductCard from "./productsInterface/ProductCard";
 
 const ProductsInterface = () => {
   const params = useParams();
@@ -9,14 +11,47 @@ const ProductsInterface = () => {
   console.log("searchParams:", searchParams);
 
   return (
-    <>
-      <Typography gutterBottom>
-        {`Categoría seleccionada: ${params["*"]}`}
-      </Typography>
-      <Typography>
-        {`Sub-categoria seleccionada: ${searchParams[0].get("subCategoria")}`}
-      </Typography>
-    </>
+    <Grid container columns="16" columnGap="20px" wrap="nowrap">
+      <GenericContainer
+        container
+        item
+        direction="column"
+        sx={{ display: { xs: "none", sm: "flex" }, height: "700px" }}
+        xs={4}
+        sm={4}
+      >
+        Criterios avanzados de bsuqeuda
+      </GenericContainer>
+
+      <GenericContainer
+        container
+        item
+        direction="column"
+        xs={16}
+        sm={12}
+        wrap="nowrap"
+      >
+        <Grid container sx={{ height: "40px", background: "white" }}>
+          Criterios de Distribucion
+        </Grid>
+
+        <Grid
+          container
+          //columnGap="20px"
+          // rowGap="20px"
+          columns="12"
+          //  sx={{ boxSizing: "border-box", padding: "20px" }}
+          //wrap="nowrap"
+        >
+          {/*xs= 2 cajas, sm= 2 cajas, md= 3 cajas, lg= 4 cajas */}
+
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </Grid>
+      </GenericContainer>
+    </Grid>
   );
 };
 
