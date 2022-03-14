fb9595be87debb1ddd44925ea04641c63591ff54
Juan-45 <quijuan12@gmail.com>
2022-03-14 12:44:54 -0300

    Update to dynamically set perspective style rule

--- src/pages/home/ParallaxContainer.js
+++ src/pages/home/ParallaxContainer.js
@@ -1,11 +1,13 @@
 import { styled } from "@mui/material/styles";
 
-const ParallaxContainer = styled("div")({
-  perspective: "1px",
+const ParallaxContainer = styled("div", {
+  shouldForwardProp: (prop) => prop !== "isMainContentURLPathSetted",
+})(({ isMainContentURLPathSetted }) => ({
+  perspective: isMainContentURLPathSetted ? "1px" : "unset",
   height: "100vh",
   width: "100vw",
   overflowX: "hidden",
   overflowY: "auto",
-});
+}));
 
 export default ParallaxContainer;
