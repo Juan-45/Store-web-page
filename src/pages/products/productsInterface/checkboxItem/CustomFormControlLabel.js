dc1be6beece12d7fbe7bdbed1d220411104eb07c
Juan-45 <quijuan12@gmail.com>
2022-03-14 14:37:10 -0300

    Create CustomFormControlLabel with checkbox
    This is used for components for advance search

---
+++ src/pages/products/productsInterface/checkboxItem/CustomFormControlLabel.js
@@ -0,0 +1,24 @@
+import { FormControlLabel, Checkbox } from "@mui/material";
+import theme from "theme";
+
+const CustomFormControlLabel = ({ sx, ...props }) => (
+  <FormControlLabel
+    control={<Checkbox />}
+    componentsProps={{
+      typography: {
+        variant: "body2",
+      },
+    }}
+    sx={{
+      margin: "0px",
+      paddingRight: "8px",
+      "&:hover": {
+        background: theme.palette.secondary.light,
+      },
+      ...sx,
+    }}
+    {...props}
+  />
+);
+
+export default CustomFormControlLabel;
