e48b971f0e9cd4743db47b534752399547a967f0
Juan-45 <quijuan12@gmail.com>
2022-03-15 09:51:24 -0300

    Update with Container

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,19 +1,13 @@
-import PageContainer from "components/PageContainer";
+import Container from "./products/Container";
 import GenericTitle from "components/GenericTitle";
 import ProductsInterface from "./products/ProductsInterface";
 
 const Products = () => {
   return (
-    <PageContainer
-      sx={{ paddingTop: { xs: "150px", sm: "230px" } }}
-      xs={11}
-      md={10}
-      lg={9}
-      xl={8}
-    >
+    <Container>
       <GenericTitle>Busca nuestros productos</GenericTitle>
       <ProductsInterface />
-    </PageContainer>
+    </Container>
   );
 };
 
