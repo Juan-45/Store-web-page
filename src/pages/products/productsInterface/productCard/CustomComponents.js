5c8a2ac05fa004c6a68239fdf726c3dffcf4b834
Juan-45 <quijuan12@gmail.com>
2022-03-19 14:02:13 -0300

    Create CustomComponents for ProductsCard

---
+++ src/pages/products/productsInterface/productCard/CustomComponents.js
@@ -0,0 +1,53 @@
+import { Grid, Box, Typography } from "@mui/material";
+import theme from "theme";
+
+const Container = ({ children }) => (
+  <Grid container items xs={6} lg={4} xl={3} justifyContent="center">
+    {children}
+  </Grid>
+);
+
+const CustomImage = ({ src }) => (
+  <Box sx={{ width: "100%", margin: "10px 10px 0px", maxWidth: "300px" }}>
+    <img src={src} alt="product" style={{ maxWidth: "100%" }} />
+  </Box>
+);
+
+const ProducstDescription = ({ title, price, details }) => (
+  <Box
+    sx={{
+      background: theme.palette.secondary.mainMedium,
+      boxSizing: "border-box",
+      padding: "15px",
+      width: "100%",
+      margin: "10px",
+      maxWidth: "300px",
+    }}
+  >
+    <Typography
+      variant="subtitle1"
+      sx={{
+        fontWeight: "600",
+        marginBottom: "1rem",
+        color: theme.palette.secondary.main,
+      }}
+    >
+      {title}
+    </Typography>
+    <Typography
+      gutterBottom
+      variant="subtitle1"
+      sx={{
+        fontWeight: "600",
+        color: theme.palette.secondary.dark,
+      }}
+    >
+      {price}
+    </Typography>
+    <Typography variant="caption" color="secondary" sx={{ fontWeight: "600" }}>
+      {details}
+    </Typography>
+  </Box>
+);
+
+export { Container, CustomImage, ProducstDescription };
