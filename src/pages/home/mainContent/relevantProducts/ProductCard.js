df140cc488d604c0bfe345eb63758b72b382ed56
Juan-45 <quijuan12@gmail.com>
2022-03-21 19:57:31 -0300

    Update with new product image

--- src/pages/home/mainContent/relevantProducts/ProductCard.js
+++ src/pages/home/mainContent/relevantProducts/ProductCard.js
@@ -1,12 +1,13 @@
 import Container from "./productCard/Container";
 import ProductImage from "./productCard/ProductImage";
 import ProductDescription from "./productCard/ProductDescription";
+import onlineStore from "images/online-store.png";
 
 const ProductCard = ({ productData }) => {
   const { title, price, minimum } = productData;
   return (
     <Container>
-      <ProductImage src={"https://via.placeholder.com/300x300"} />
+      <ProductImage src={onlineStore} />
       <ProductDescription title={title} price={price} minimum={minimum} />
     </Container>
   );
