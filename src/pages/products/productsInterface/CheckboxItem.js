081d7d70d5f7a57100ac4562905f1a4438feecd1
Juan-45 <quijuan12@gmail.com>
2022-03-21 22:11:11 -0300

    Update with the new hook implementation

--- src/pages/products/productsInterface/CheckboxItem.js
+++ src/pages/products/productsInterface/CheckboxItem.js
@@ -1,24 +1,25 @@
 import CustomFormControlLabel from "./checkboxItem/CustomFormControlLabel";
-import { useState, useEffect } from "react";
+import useCheckBoxItem from "./checkboxItem/useCheckBoxItem";
 
-const CheckboxItem = ({ shouldReset, manageQueries, ...props }) => {
-  const [checked, setChecked] = useState(false);
-
-  const handleCheckbox = (e) => {
-    manageQueries(e);
-    setChecked((prev) => !prev);
-  };
-
-  useEffect(() => {
-    if (shouldReset) {
-      setChecked(false);
-    }
-  }, [shouldReset]);
+const CheckboxItem = ({
+  shouldReset,
+  manageQueries,
+  value,
+  queryKey,
+  ...props
+}) => {
+  const { checked, handleCheckbox } = useCheckBoxItem({
+    shouldReset,
+    manageQueries,
+    value,
+    queryKey,
+  });
 
   return (
     <CustomFormControlLabel
       checked={checked}
       onChange={handleCheckbox}
+      value={value}
       {...props}
     />
   );
