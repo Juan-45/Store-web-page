5c36be4b0d72ef626391f1c7559eb81f65a5ffe9
Juan-45 <quijuan12@gmail.com>
2022-03-25 18:19:52 -0300

    Remove shadows

--- src/components/GenericContainer.js
+++ src/components/GenericContainer.js
@@ -3,7 +3,7 @@ import theme from "theme";
 
 const GenericContainer = styled(Grid)({
   background: theme.palette.secondary.light,
-  boxShadow: theme.shadows[16],
+  boxShadow: theme.shadows[0],
   border: theme.borders.main,
   boxSizing: "border-box",
 });
