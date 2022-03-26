2a62bff7627487b2670b47b4ec4c5a369dedde18
Juan-45 <quijuan12@gmail.com>
2022-03-25 21:31:16 -0300

    Remove background

--- src/components/GenericContainer.js
+++ src/components/GenericContainer.js
@@ -2,7 +2,6 @@ import { Grid, styled } from "@mui/material";
 import theme from "theme";
 
 const GenericContainer = styled(Grid)({
-  background: theme.palette.secondary.light,
   boxShadow: theme.shadows[0],
   border: theme.borders.main,
   boxSizing: "border-box",
