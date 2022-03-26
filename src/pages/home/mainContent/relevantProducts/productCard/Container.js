76b2ef7c4784ff9813762c163a901712fd035038
Juan-45 <quijuan12@gmail.com>
2022-03-25 22:07:06 -0300

    Add filter effect on hover

--- src/pages/home/mainContent/relevantProducts/productCard/Container.js
+++ src/pages/home/mainContent/relevantProducts/productCard/Container.js
@@ -6,6 +6,9 @@ const Container = ({ children }) => (
       padding: 25,
       boxSizing: "border-box",
       height: "fit-content",
+      "&:hover": {
+        filter: "brightness(90%)",
+      },
     }}
   >
     <Paper elevation={8} square={true} sx={{ background: "unset" }}>
